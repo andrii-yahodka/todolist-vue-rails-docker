c0821dc3adca46a02d9a735914f86ad68e054bf0
Andrii Yahodka <andrei.iagodka@gmail.com>
2024-07-18 21:12:54 +0300

    Add update and destroy actions

--- backend/app/controllers/api/v1/tasks_controller.rb
+++ backend/app/controllers/api/v1/tasks_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::TasksController < ApplicationController
+  before_action :set_task, only: [:update, :destroy]
+
   def index
     render json: Task.all
   end
 
   def create
-    task = Task.new(create_params)
+    task = Task.new(save_params)
 
     if task.save
       render json: task, status: :created
@@ -13,9 +15,27 @@ class Api::V1::TasksController < ApplicationController
     end
   end
 
+  def update
+    if @task.update(save_params)
+      render json: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+
+    head :no_content
+  end
+
   private
 
-  def create_params
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def save_params
     params.require(:task).permit(:title, :description, :due_date)
   end
 end
