60ed4a9cc03fc9e87743101926c53c07af5368b2
Andrii Yahodka <andrei.iagodka@gmail.com>
2024-07-18 21:06:17 +0300

    Add create action

--- backend/app/controllers/api/v1/tasks_controller.rb
+++ backend/app/controllers/api/v1/tasks_controller.rb
@@ -2,4 +2,20 @@ class Api::V1::TasksController < ApplicationController
   def index
     render json: Task.all
   end
+
+  def create
+    task = Task.new(create_params)
+
+    if task.save
+      render json: task, status: :created
+    else
+      render json: task.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:task).permit(:title, :description, :due_date)
+  end
 end
