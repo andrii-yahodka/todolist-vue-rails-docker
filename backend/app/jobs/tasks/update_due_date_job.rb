5ac0436524a303aabce8aecc02cf104a4f26a38f
Andrii Yahodka <andrei.iagodka@gmail.com>
2024-08-06 16:55:13 +0300

    Rethink args usage at Sidekiq job

--- backend/app/jobs/tasks/update_due_date_job.rb
+++ backend/app/jobs/tasks/update_due_date_job.rb
@@ -2,6 +2,6 @@ module Tasks
   class UpdateDueDateJob < ApplicationJob
     queue_as :default
 
-    def perform(due_date) = Task.update_all(due_date: due_date)
+    def perform(args) = Task.update_all(due_date: args[:due_date])
   end
 end
